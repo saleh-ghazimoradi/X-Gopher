b7e9310c7128510245e98368c0c4ad00691def5c
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-20 22:33:35 +0330

    update user dto

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,5 +1,14 @@
 package dto
 
+import "github.com/saleh-ghazimoradi/X-Gopher/internal/helper"
+
+type UpdateUserReq struct {
+	FirstName *string `json:"first_name"`
+	LastName  *string `json:"last_name"`
+	ImageUrl  *string `json:"image_url"`
+	Bio       *string `json:"bio"`
+}
+
 type UserResp struct {
 	Id        string   `json:"id"`
 	FirstName string   `json:"first_name"`
@@ -9,4 +18,28 @@ type UserResp struct {
 	Bio       string   `json:"bio"`
 	Followers []string `json:"followers"`
 	Following []string `json:"following"`
+}
+
+func validateImageUrl(v *helper.Validator, image string) {
+
+}
+
+func validateBio(v *helper.Validator, bio string) {
+	v.Check(len(bio) <= 72, "bio", "bio length must not be greater than 72")
+}
+
+func ValidateUpdateUserReq(v *helper.Validator, req *UpdateUserReq) {
+	if req.FirstName != nil {
+		validateFirstName(v, *req.FirstName)
+	}
+	if req.LastName != nil {
+		validateLastName(v, *req.LastName)
+	}
+
+	if req.ImageUrl != nil {
+		validateImageUrl(v, *req.ImageUrl)
+	}
+	if req.Bio != nil {
+		validateBio(v, *req.Bio)
+	}
 }
