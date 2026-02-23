feb992a315a21834c6f238d395c2b4e2939d2cfb
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-23 12:51:59 +0330

    add custom errors

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrDuplicateEmail   = errors.New("duplicate email")
 	ErrRecordNotFound   = errors.New("record not found")
 	ErrCannotFollowSelf = errors.New("cannot follow yourself")
+	ErrInvalidId        = errors.New("invalid id")
+	ErrUnauthorized     = errors.New("unauthorized action")
 )
