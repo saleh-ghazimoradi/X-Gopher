00cc83c7f29d5dfcbfd4ebe6dea0f8ef2b522505
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-23 16:49:06 +0330

    update unreadmessage repository

--- internal/repository/unreadMessage.go
+++ internal/repository/unreadMessage.go
@@ -17,23 +17,20 @@ type unreadMessageRepository struct {
 }
 
 func (u *unreadMessageRepository) IncrementUnread(ctx context.Context, senderId, receiverId string) error {
-	{
-
-		filter := bson.M{"sender_id": senderId, "receiver_id": receiverId}
-		update := bson.M{
-			"$inc": bson.M{"num_of_unread_messages": 1},
-			"$set": bson.M{"is_read": false},
-		}
-
-		opts := options.FindOneAndUpdate().
-			SetUpsert(true).
-			SetReturnDocument(options.After)
-
-		err := u.collection.FindOneAndUpdate(ctx, filter, update, opts).
-			Decode(new(domain.UnreadMessage))
-
-		return err
+	filter := bson.M{"sender_id": senderId, "receiver_id": receiverId}
+	update := bson.M{
+		"$inc": bson.M{"num_of_unread_messages": 1},
+		"$set": bson.M{"is_read": false},
 	}
+
+	opts := options.FindOneAndUpdate().
+		SetUpsert(true).
+		SetReturnDocument(options.After)
+
+	err := u.collection.FindOneAndUpdate(ctx, filter, update, opts).
+		Decode(new(domain.UnreadMessage))
+
+	return err
 }
 
 func NewUnreadMessageRepository(database *mongo.Database, collectionName string) UnreadMessageRepository {
